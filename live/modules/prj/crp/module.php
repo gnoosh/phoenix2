3ff80154280bf54454fd4d62ed211cabcf0e89df
one-trick-pony <linus.manser@gmail.com>
2016-09-07 18:58:20 +0200

    Add files via upload

--- live/modules/prj/crp/module.php
+++ live/modules/prj/crp/module.php
@@ -55,7 +55,7 @@ function reopenAfterPageRefresh (corpusID) {
 
 // routine
 $(document).ready( function() {
-	
+
 	// show corpus details / assigned texts
 	$("table#corpora tbody tr").click( function() {
 		// select corpusID from checkbox in same tr
@@ -64,13 +64,13 @@ $(document).ready( function() {
 		showCorpusDetailsBox(this, corpusID, true);
 		showAssignedTextBox(corpusID, true);
 	});
-	
+
 	// switch checked-in to checked-out when the check-out symbol is clicked
 	$("a.checkout").live('click', function() {
 		$(this).toggleClass('invisible');
 		$(this).parent().find('a.checkin').toggleClass('invisible');
 	});
-	
+
 	// handle corpus update actions (form)
 	$('#update_corpora').submit( function(e) {
 		if ($('#corpora_action').val() == 'delete' ) {
@@ -81,7 +81,7 @@ $(document).ready( function() {
         	});
 		}
 	});
-	
+
 	// handle text order number input fields
 	var wto;
 	$("input.hybrid.textordernumber").live('keypress', function(e) {
@@ -98,11 +98,11 @@ $(document).ready( function() {
 			}, 1000);
 		}
 	});
-	
+
 	$("input.hybrid.textordernumber").live('focusout', function() {
 		updateTextOrderNumber($(this));
 	});
-	
+
 	function updateTextOrderNumber (input_object) {
 	// updates the order number of a text, given a JQuery object of the corresponding <input> element
 		var text_id = input_object.attr('name').split('-').slice(-1)[0];
@@ -119,7 +119,7 @@ $(document).ready( function() {
 			input_object.addClass('invalid');
 		}
 	}
-	
+
 	// session - reopen modules after page refresh
 	<?php if($_POST['corpus_details'] || $_POST['add_corpus']) {
 		$corpus_id = $_POST['corpus_id'];
@@ -127,7 +127,7 @@ $(document).ready( function() {
 		echo "reopenAfterPageRefresh($corpus_id);";
 	}
 	?>
-	
+
 	// REMOVE LOADING PAGE INDICATOR
 	$('#loading').fadeOut();
 });
@@ -159,22 +159,22 @@ $(document).ready( function() {
             	<?php
 				$resultset_corpora = $project->getAssignedCorpora($as_resultset=TRUE);
 				// actions for each corpus (export XML, checkout)
-				
+
 				// get the IDs of all Corpora which are currently checked out
 				$dao = new Table ('CHECKOUT');
 				$checked_out_corpora = $dao->get( "CorpusID is not NULL and Checkin is NULL and IsInvalid=0" );
 				$checked_out_corpus_ids = array();
 				foreach($checked_out_corpora as $checked_out_corpus) {
 					$checked_out_corpus_ids[] = $checked_out_corpus['CorpusID'];
-				}			
-				
+				}
+
 				$resultset_corpora_with_actions = array();
 				foreach ($resultset_corpora as $r_corpus) {
 					$corpus_id = $r_corpus['ID'];
 					$corpus_name = $r_corpus['Name'];
 					$actions = '<a class="icon inline download" title="Download ' . $corpus_name . ' as XML file (STORRAGE format)." href="?action=DownloadXMLCorpus&corpus_id=' . $corpus_id . '"></a>';
 					//check whether this corpus is checked-out
-					
+
 					$hidden_checkin = '';
 					$hidden_checkout = '';
 					if (in_array($corpus_id, $checked_out_corpus_ids)) {
@@ -182,13 +182,13 @@ $(document).ready( function() {
 					} else {
 						$hidden_checkin = ' invisible';
 					}
-					
+
 					$actions .= '<a class="icon inline def checkin' . $hidden_checkin . '" title="' . $corpus_name . ' has been exported for external editing. Click here to check-in the edited XML file (EDIT format)." href="modal.php?modal=import&import_corpus_id=' . $corpus_id . '" rel="facebox"></a>';
 					$actions .= '<a id="checkout-corpus-' . $corpus_id . '"class="icon inline abc checkout' . $hidden_checkout . '" title="Export ' . $corpus_name . ' as XML file for external editing (EDIT format)." href="modal.php?modal=export&type=corpus&id=' . $corpus_id . '&name=' . $corpus_name . '" rel="facebox"></a>';
 					//add the new entry to the resultset
 					$resultset_corpora_with_actions[] = array( 'ID' => $corpus_id, 'Actions' => $actions, 'Name' => $corpus_name, 'Description' => $r_corpus['Description'], '# Texts' => $r_corpus['# Texts'] );
 				}
-				
+
 				$tr = new ResultSetTransformer($resultset_corpora_with_actions);
 				echo $tr->toSelectableHTMLTable('all', 'ID', 'corpus_id', 'corpora');
 				?>
@@ -196,7 +196,7 @@ $(document).ready( function() {
             </div>
         </div>
     </div>
-    
+
     <div class="w33 right">
         <div class="modulebox">
             <div class="title">Corpus Properties</div>
@@ -218,7 +218,7 @@ $(document).ready( function() {
             </div>
         </div>
     </div>
-    
+
     <div class="w66 hidden" id="assigned_texts">
         <div class="modulebox">
             <div class="title">Assigned Texts</div>
@@ -242,20 +242,20 @@ $(document).ready( function() {
 					// get all texts assigned to a corpus
 					$dao = new Table('TEXT');
 					/* SQL query:
-					select t.TextID, CiteID, d.d0, r.rd0 from TEXT as t 
+					select t.TextID, CiteID, d.d0, r.rd0 from TEXT as t
 					join (select TextID, Value as d0 from DESCRIPTOR natural join TEXT_DESCRIPTOR where XMLTagName='d0') as d on t.TextID=d.TextID
 					join (select TextID, Value as rd0 from DESCRIPTOR natural join TEXT_DESCRIPTOR where XMLTagName='rd0') as r on t.TextID=r.TextID
 					where CorpusID=30
 					*/
-					
+
 					// get Texts from DB
 					$dao->select = "t.TextID, t.Order, CiteID, d.d0, r.rd0";
-					$dao->from = "TEXT as t 
+					$dao->from = "TEXT as t
 					join (select TextID, Value as d0 from DESCRIPTOR natural join TEXT_DESCRIPTOR where XMLTagName='d0') as d on t.TextID=d.TextID
 					join (select TextID, Value as rd0 from DESCRIPTOR natural join TEXT_DESCRIPTOR where XMLTagName='rd0') as r on t.TextID=r.TextID";
 					$dao->orderby = "t.Order, CAST(SUBSTRING(t.CiteID,LOCATE(' ',t.CiteID)+1) AS SIGNED) ASC";
 					$results = $dao->get( array('CorpusID' => $corpus->getID()) );
-					
+
 					// get the IDs of all Texts which are currently checked out
 					/* SQL QUERY
 					select * from CHECKOUT where TextID is not NULL and Checkin is NULL and IsInvalid=0
@@ -266,15 +266,15 @@ $(document).ready( function() {
 					foreach($checked_out_texts as $checked_out_text) {
 						$checked_out_text_ids[] = $checked_out_text['TextID'];
 					}
-					
+
 					// insert links for facebox-popup (text display)
-					$preset_order_nr = 1;
 					$results_with_links = array();
+					$last_order_nr = 0;
 					foreach ($results as $row) {
 						// actions
 						$actions = '';
 						$actions .= '<a class="icon inline download" title="Download ' . $row['CiteID'] . ' as XML file (STORRAGE format)." href="?action=DownloadXMLText&text_id=' . $row['TextID'] . '"></a>';
-						
+
 						$hidden_checkin = '';
 						$hidden_checkout = '';
 						if (in_array($row['TextID'], $checked_out_text_ids)) {
@@ -282,7 +282,7 @@ $(document).ready( function() {
 						} else {
 							$hidden_checkin = ' invisible';
 						}
-						
+
 						$actions .= '<a class="icon inline def checkin' . $hidden_checkin . '" title="' . $row['CiteID'] . ' has been exported for external editing. Click here to check-in the edited XML file (EDIT format)." href="modal.php?modal=import&import_text_id=' . $row['TextID'] . '" rel="facebox"></a>';
 						$actions .= '<a id="checkout-text-' . $row['TextID'] . '" class="icon inline abc checkout' . $hidden_checkout . '" title="Export ' . $row['CiteID'] . ' as XML file for external editing (EDIT format)." href="modal.php?modal=export&type=text&id=' . $row['TextID'] . '&name=' . $row['CiteID'] . '" rel="facebox"></a>';
 						// information
@@ -296,9 +296,16 @@ $(document).ready( function() {
 						 EV. TODO: if order numbers should stay once they got edited, some editing is needed */
 						//if ($order_nr == null)
 						//$results_with_links[] = array('ID' => $row['TextID'], 'Ord.' => '<div style="width:60px;"><input name="order-number-text-'.$row['TextID'].'" class="hybrid textordernumber left" value="'.$order_nr.'" tabindex="'.$tabindex.'" /><a class="icon ok inline right" style="display:none"  title="saved">&nbsp;</a></div>', 'Actions' => $actions, 'CiteID' => $id, 'Date' => $d0, 'Editor' => $rd0);
-						$results_with_links[] = array('ID' => $row['TextID'], 'Ord.' => '<div style="width:60px;"><input name="order-number-text-'.$row['TextID'].'" class="hybrid textordernumber left" value="'.$preset_order_nr.'" tabindex="'.$tabindex.'" /><a class="icon ok inline right" style="display:none"  title="saved">&nbsp;</a></div>', 'Actions' => $actions, 'CiteID' => $id, 'Date' => $d0, 'Editor' => $rd0);
+						if (empty($order_nr)) {
+							$preset_order_nr = $last_order_nr + 1;
+							$results_with_links[] = array('ID' => $row['TextID'], 'Ord.' => '<div style="width:60px;"><input name="order-number-text-'.$row['TextID'].'" class="hybrid textordernumber left" value="'.$preset_order_nr.'" tabindex="'.$tabindex.'" /><a class="icon ok inline right" style="display:none"  title="saved">&nbsp;</a></div>', 'Actions' => $actions, 'CiteID' => $id, 'Date' => $d0, 'Editor' => $rd0);
+							$last_order_nr = $preset_order_nr;
+						} else {
+							$results_with_links[] = array('ID' => $row['TextID'], 'Ord.' => '<div style="width:60px;"><input name="order-number-text-'.$row['TextID'].'" class="hybrid textordernumber left" value="'.$order_nr.'" tabindex="'.$tabindex.'" /><a class="icon ok inline right" style="display:none"  title="saved">&nbsp;</a></div>', 'Actions' => $actions, 'CiteID' => $id, 'Date' => $d0, 'Editor' => $rd0);
+							$last_order_nr = $order_nr;
+						}
 						$tabindex++;
-						$preset_order_nr++;
+
 					}
 					// print the html
 					$transformer = new ResultSetTransformer($results_with_links);
